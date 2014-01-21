10a083dd9453a8477a21cce0e7bf8ca48abf68c0
Pascal van Gemert <pascal@pascalvangemert.nl>
2014-01-21 17:16:56 +0100

    Updated json

--- view/sections/profile.inc.php
+++ view/sections/profile.inc.php
@@ -23,5 +23,8 @@
 			<strong>Location:</strong><br />
 			<?= $profile->current_location->city; ?>, <?= $profile->current_location->country; ?>, <?= $profile->current_location->planet; ?>
 		</p>
+		
+		<a href="https://twitter.com/pascalvgemert" class="twitter-follow-button" data-show-count="false" data-size="large" data-show-screen-name="false">Follow @pascalvgemert</a>
+		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 	</div>
 </div>
